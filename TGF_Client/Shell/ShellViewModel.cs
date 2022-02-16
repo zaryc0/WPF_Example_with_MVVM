11f6f5f5f586de70bf1755b2bceaa08f56e97400
Zaryc0 <cotgray1997@gmail.com>
2022-02-16 11:43:50 +0000

    ammended resources to include datatemplates and styles overrides aswell as the merged dictionary

---
+++ TGF_Client/Shell/ShellViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TGF_Client.Shell
+{
+    class ShellViewModel
+    {
+    }
+}
---
+++ TGF_Client/ViewModel/UserTextInputVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TGF_Client.ViewModel
+{
+    class UserTextInputVM : BaseViewModel
+    {
+        //properties
+        public string Text { get; set; }
+
+        //constructor
+        public UserTextInputVM()
+        {
+            Text = "Text...";
+        }
+
+    }
+}
